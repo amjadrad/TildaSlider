9cf7498fe8cfddb11fec536f4cca275df9e61ecf
Amjad Qasemie Rad <amjadrad@users.noreply.github.com>
2025-02-17 18:11:40 +0330

    Update gradle and libraries

--- tilda_slider/src/main/java/ir/tildaweb/tilda_slider/views/fragments/PosterFragment.java
+++ tilda_slider/src/main/java/ir/tildaweb/tilda_slider/views/fragments/PosterFragment.java
@@ -1,41 +1,34 @@
 package ir.tildaweb.tilda_slider.views.fragments;
 
 
-import static com.google.android.exoplayer2.Player.STATE_ENDED;
+import static androidx.media3.common.Player.STATE_ENDED;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.OptIn;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.PlaybackParameters;
+import androidx.media3.common.Player;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.datasource.DataSource;
+import androidx.media3.datasource.DataSpec;
+import androidx.media3.datasource.DefaultHttpDataSource;
+import androidx.media3.datasource.RawResourceDataSource;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.exoplayer.source.MediaSource;
+import androidx.media3.exoplayer.source.ProgressiveMediaSource;
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
+import androidx.media3.exoplayer.trackselection.TrackSelector;
+import androidx.media3.ui.PlayerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.transition.Transition;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DataSpec;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
-import com.google.android.exoplayer2.upstream.RawResourceDataSource;
 
-import ir.tildaweb.tilda_slider.MathUtils;
 import ir.tildaweb.tilda_slider.events.IVideoPlayListener;
 import ir.tildaweb.tilda_slider.events.OnPosterClickListener;
 import ir.tildaweb.tilda_slider.posters.BitmapImage;
@@ -49,9 +42,9 @@ import ir.tildaweb.tilda_slider.posters.VideoPoster;
 import ir.tildaweb.tilda_slider.views.AdjustableImageView;
 import ir.tildaweb.tilda_slider.views.TildaSlider;
 
+@UnstableApi
 public class PosterFragment extends Fragment implements Player.Listener {
 
-    private final String TAG = this.getClass().getName();
     private Poster poster;
 
     private IVideoPlayListener videoPlayListener;
@@ -80,26 +73,24 @@ public class PosterFragment extends Fragment implements Player.Listener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        poster = getArguments().getParcelable("poster");
+        poster = getArguments() != null ? getArguments().getParcelable("poster") : null;
     }
 
+    @OptIn(markerClass = UnstableApi.class)
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        if (poster != null) {
-            if (poster instanceof ImagePoster) {
+        if (poster != null && getActivity() != null) {
+            if (poster instanceof ImagePoster imagePoster) {
                 final AdjustableImageView imageView = new AdjustableImageView(getActivity());
                 imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 imageView.setAdjustViewBounds(true);
-                ImagePoster imagePoster = (ImagePoster) poster;
                 imageView.setScaleType(imagePoster.getScaleType());
-                if (imagePoster instanceof DrawableImage) {
-                    DrawableImage image = (DrawableImage) imagePoster;
+                if (imagePoster instanceof DrawableImage image) {
                     Glide.with(getActivity())
                             .load(image.getDrawable())
                             .into(imageView);
-                } else if (imagePoster instanceof BitmapImage) {
-                    BitmapImage image = (BitmapImage) imagePoster;
+                } else if (imagePoster instanceof BitmapImage image) {
                     Glide.with(getActivity())
                             .load(image.getBitmap())
                             .into(imageView);
@@ -180,7 +171,7 @@ public class PosterFragment extends Fragment implements Player.Listener {
                 return imageView;
             } else if (poster instanceof VideoPoster) {
                 final PlayerView playerView = new PlayerView(getActivity());
-                BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+//                BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
 //                TrackSelection.Factory videoTrackSelectionFactory =
 //                        new AdaptiveTrackSelection.Factory(bandwidthMeter);
                 TrackSelector trackSelector =
@@ -194,8 +185,7 @@ public class PosterFragment extends Fragment implements Player.Listener {
                     playerView.setUseController(false);
                 }
 
-                if (poster instanceof RawVideo) {
-                    RawVideo video = (RawVideo) poster;
+                if (poster instanceof RawVideo video) {
                     DataSpec dataSpec = new DataSpec(RawResourceDataSource.buildRawResourceUri(video.getRawResource()));
                     final RawResourceDataSource rawResourceDataSource = new RawResourceDataSource(getActivity());
                     try {
@@ -207,9 +197,8 @@ public class PosterFragment extends Fragment implements Player.Listener {
                     DataSource.Factory factory = () -> rawResourceDataSource;
                     ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(rawResourceDataSource.getUri()));
                     player.prepare(mediaSource);
-                } else if (poster instanceof RemoteVideo) {
-                    RemoteVideo video = (RemoteVideo) poster;
-//                    MediaSource mediaSource = new ProgressiveMediaSource.Factory(
+                } else if (poster instanceof RemoteVideo video) {
+                    //                    MediaSource mediaSource = new ProgressiveMediaSource.Factory(
 //                            new DefaultHttpDataSourceFactory(Util.getUserAgent(getActivity(), "TildaSlider"))).
 //                            createMediaSource(video.getUri());
                     MediaSource mediaSource = new ProgressiveMediaSource.Factory(new DefaultHttpDataSource.Factory()).createMediaSource(MediaItem.fromUri(video.getUri()));
@@ -235,27 +224,7 @@ public class PosterFragment extends Fragment implements Player.Listener {
     }
 
     @Override
-    public void onRepeatModeChanged(int repeatMode) {
-
-    }
-
-    @Override
-    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
-
-    }
-
-    @Override
     public void onPositionDiscontinuity(int reason) {
-
-    }
-
-    @Override
-    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
-    }
-
-    @Override
-    public void onSeekProcessed() {
 
     }
 
@@ -271,5 +240,4 @@ public class PosterFragment extends Fragment implements Player.Listener {
             player.addListener(this);
         }
     }
-
 }
